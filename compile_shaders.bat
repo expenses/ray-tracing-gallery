a624c961d7ef9d4b215eb31984da5d190c6136be
Ashley <ashley.ruglys@gmail.com>
2021-10-29 12:34:01 +0200

    Updated rust-gpu

--- compile_shaders.bat
+++ compile_shaders.bat
@@ -1,6 +1,6 @@
 :: Keep this target up-to-date with the rust-gpu one.
-:: rustup +nightly-2021-09-29-x86_64-pc-windows-msvc component add rust-src rustc-dev llvm-tools-preview
-cargo +nightly-2021-09-29-x86_64-pc-windows-msvc run -p compile-shaders --release
+:: rustup +nightly-2021-10-26-pc-windows-msvc component add rust-src rustc-dev llvm-tools-preview
+cargo +nightly-2021-10-26-x86_64-pc-windows-msvc run -p compile-shaders --release
 
 glslc --target-spv=spv1.4 -fshader-stage=rchit shaders/closest_hit_textured.glsl -o shaders/closest_hit_textured.spv
 glslc --target-spv=spv1.4 -fshader-stage=rchit shaders/closest_hit_mirror.glsl -o shaders/closest_hit_mirror.spv
